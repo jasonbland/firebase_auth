0149b7b11bed3fcc9c4e31389f0b26771eb225b5
Jason Bland <jtbland@gmail.com>
2018-11-16 23:53:09 -0800

    add controlled input and submit handler

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -3,12 +3,18 @@ import { Text, View } from 'react-native';
 import { Button, FormInput, FormLabel } from 'react-native-elements';
 
 class SignUpForm extends Component {
+  state = { phone: '' };
+
+  handleSubmit = () => {};
+
   render() {
     return (
       <View>
-        <FormLabel>Enter Phone Number</FormLabel>
-        <FormInput />
-        <Button title="Submit" />
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Phone Number</FormLabel>
+          <FormInput value={this.state.phone} onChangeText={phone => this.setState({ phone })} />
+        </View>
+        <Button onPress={this.handleSubmit} title="Submit" />
       </View>
     );
   }
