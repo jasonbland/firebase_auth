1afc0a306c26e455f0a6b72ca6671926a7dd92bf
Jason Bland <jtbland@gmail.com>
2018-11-17 22:32:03 -0800

    reposition inputs

--- App.js
+++ App.js
@@ -19,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-around'
   }
 });
