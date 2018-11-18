6f2ede9149434be7714bd8ad6df36aa215d7c29f
Jason Bland <jtbland@gmail.com>
2018-11-17 22:09:04 -0800

    add code input and handleRequest request

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -5,8 +5,16 @@ import axios from 'axios';
 
 const ROOT_URL = 'https://us-central1-one-time-password-296fa.cloudfunctions.net/';
 
+handleSubmit = async () => {
+  try {
+    await axios.post(`${ROOT_URL}verifyOneTimePassword`, { phone: this.state.phone, code: this.state.code });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 class SignInForm extends Component {
-  state = { phone: '' };
+  state = { phone: '', code: '' };
 
   render() {
     return (
@@ -14,6 +22,10 @@ class SignInForm extends Component {
         <View style={{ marginBottom: 10 }}>
           <FormLabel>Enter Phone Number</FormLabel>
           <FormInput value={this.state.phone} onChangeText={phone => this.setState({ phone })} />
+        </View>
+        <View style={{ marginBottom: 10 }}>
+          <FormLabel>Enter Code</FormLabel>
+          <FormInput value={this.state.code} onChangeText={code => this.setState({ code })} />
         </View>
         <Button onPress={this.handleSubmit} title="Submit" />
       </View>
